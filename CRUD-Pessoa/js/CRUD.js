e5d61c72de6eb2f814a330b5e2d5a1480f985af7
Jeferson Nolasco <Jeferson Nolasco@sindeepresorg.local>
2020-03-03 16:08:12 -0300

    Arruamndo as requisicoes

---
+++ CRUD-Pessoa/js/CRUD.js
@@ -0,0 +1,85 @@
+$("#formCadastro").click(function () {
+
+   var nome = $('#nome').val();
+   var sobrenome = $('#sobrenome').val();
+   var email = $('#email').val();
+
+   var pessoa = { id: '', nome: nome, sobrenome: sobrenome, email: email };
+   var dataPessoa = JSON.stringify(pessoa);
+
+   $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/api/pessoa/cadastrar",
+      data: dataPessoa,
+      contentType: "application/json",
+      sucesso: deuCerto,
+      erro: deuErrado
+   });
+
+   function deuCerto() {
+      alert("Cadastrado com sucesso !!")
+   }
+
+   function deuErrado() {
+      alert("erro")
+   }
+
+});
+
+
+$("#bntListaDePessoas").click(function () {
+   $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/api/pessoa/listar",
+      success: deuCerto,
+      error: deuErro,
+      dataType: "json"
+
+   });
+
+   function deuCerto(response) {
+      popularLista(response)
+
+      console.log(pessoa)
+      alert(pessoa[0].nome)
+   }
+   function deuErro(response) {
+      alert("erro")
+   }
+
+
+
+});
+
+function popularLista(ListPessoas) {
+   let container = document.querySelector("#listaDePessoas");
+   for (i = '0'; i < ListPessoas.length; i++) {
+      container = insertAdjacentHTML("beforeend", organizarLista(ListPessoas[i].nome, ListPessoas[i].sobrenome, ListPessoas[i].email, ));
+   }
+}
+
+function organizarLista(nome, sobrenome, email) {
+   return "<th>" +
+      "<td> " + nome + " </td> " +
+      "<td> " + sobrenome + " </td> " +
+      "<td>" + email + "</td>" +
+      "</th>"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

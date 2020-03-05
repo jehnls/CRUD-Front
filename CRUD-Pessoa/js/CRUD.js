667b66f61fd700bd98721560bd75a7f0e1631f4d
Jeferson Nolasco <Jeferson Nolasco@sindeepresorg.local>
2020-03-05 16:52:35 -0300

    foi ditado o estilo da lista de pessoas

--- CRUD-Pessoa/js/CRUD.js
+++ CRUD-Pessoa/js/CRUD.js
@@ -1,81 +1,74 @@
-$("#formCadastro").click(function () {
+$("#formCadastro").click(function() {
 
-   var nome = $('#nome').val();
-   var sobrenome = $('#sobrenome').val();
-   var email = $('#email').val();
+    var nome = $('#nome').val();
+    var sobrenome = $('#sobrenome').val();
+    var email = $('#email').val();
 
-   var pessoa = { id: '', nome: nome, sobrenome: sobrenome, email: email };
-   var dataPessoa = JSON.stringify(pessoa);
+    var pessoa = { id: '', nome: nome, sobrenome: sobrenome, email: email };
+    var dataPessoa = JSON.stringify(pessoa);
 
-   $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/api/pessoa/cadastrar",
-      data: dataPessoa,
-      contentType: "application/json",
-      sucesso: deuCerto,
-      erro: deuErrado
-   });
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/pessoa/cadastrar",
+        data: dataPessoa,
+        contentType: "application/json",
+        sucesso: deuCerto,
+        erro: deuErrado
+    });
 
-   function deuCerto() {
-      alert("Cadastrado com sucesso !!")
-   }
+    function deuCerto() {
+        alert("Cadastrado com sucesso !!")
+    }
 
-   function deuErrado() {
-      alert("erro")
-   }
+    function deuErrado() {
+        alert("erro")
+    }
 
 });
 
+var mesmaResposta = null;
 
-$("#bntListaDePessoas").click(function () {
-   $.ajax({
-      type: "GET",
-      url: "http://localhost:8080/api/pessoa/listar",
-      success: deuCerto,
-      error: deuErro,
-      dataType: "json"
 
-   });
+$("#bntListaDePessoas").click(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/pessoa/listar",
+        success: deuCerto,
+        error: deuErro,
+        dataType: "json"
 
-   function deuCerto(response) {
-      popularLista(response)
+    });
 
-   }
-   function deuErro(response) {
-      alert("erro")
-   }
+    function deuCerto(response) {
+        if (mesmaResposta == null) {
+            mesmaResposta = response;
+            popularLista(response)
+        } else {
+            alert("Nao exite mais pessoas na base de dados")
+        }
+
+
+    }
+
+    function deuErro(response) {
+        alert("erro" + response)
+    }
 
 });
 
 function popularLista(ListPessoas) {
-   let container = document.getElementById("listaDePessoas");
-   for (i = '0'; i < ListPessoas.length; i++) {
-      container.insertAdjacentHTML("beforeend", organizarLista(ListPessoas[i].nome, ListPessoas[i].sobrenome, ListPessoas[i].email));
-   }
+    let container = document.getElementById("listaDePessoas");
+    for (i = '0'; i < ListPessoas.length; i++) {
+        container.insertAdjacentHTML("beforeend", organizarLista(ListPessoas[i].nome, ListPessoas[i].sobrenome, ListPessoas[i].email));
+    }
 }
 
 function organizarLista(nome, sobrenome, email) {
-   return "<th>" +
-      "<td> " + nome + " </td> " +
-      "<td> " + sobrenome + " </td> " +
-      "<td>" + email + "</td>" +
-      "</th>"
+    return "<tr>" +
+        "<td> " + nome + " </td> " +
+        "<td> " + sobrenome + " </td> " +
+        "<td>" + email + "</td>" +
+        "<td> <button>Alterar</button>" + "</td>" +
+        "<td> <button>Excluir</button>" + "</td>" +
+        "</tr>"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
